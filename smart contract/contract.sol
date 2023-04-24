2d0e012900f3e92cf14d997e91c22f387da61742
tommaso caputi <tommasocaputi85@gmail.com>
2023-04-24 15:09:21 +0200

    init contract

--- smart contract/contract.sol
+++ smart contract/contract.sol
@@ -2,5 +2,38 @@
 pragma solidity >=0.4.0 <0.9.0;
 
 contract DSMContract {
+    struct account {
+        string nickname;
+        uint256 creationDate;
+        string bio;
+        string imgIpfs;
+        string name;
+        string surname;
+    }
+
+    mapping(address => account) public accounts;
+    mapping(address => string[]) public posts;
+
+    function createAccount(
+        string memory _nickname,
+        uint256 _creationDate,
+        string memory _bio,
+        string memory _imgIpfs,
+        string memory _name,
+        string memory _surname
+    ) public {
+        accounts[msg.sender] = account(
+            _nickname,
+            _creationDate,
+            _bio,
+            _imgIpfs,
+            _name,
+            _surname
+        );
+    }
+
+    function getAccount(address _address) public view returns (account memory) {
+        return accounts[_address];
+    }
     
 }
